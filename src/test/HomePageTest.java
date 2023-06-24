097f2ad7f58785754c0b9868c7743e0231065433
ashishk2126 <ashishk2126@gmail.com>
2023-06-24 09:55:43 +0530

    test first commit

---
+++ src/test/HomePageTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class HomePageTest {
+	public void test2()
+	{
+		System.out.println("hi this is the test homepage");
+	}
+}
---
+++ src/test/LoginPageTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class LoginPageTest {
+	public void test1()
+	{
+		System.out.println("This is the test login page method");
+	}
+}
